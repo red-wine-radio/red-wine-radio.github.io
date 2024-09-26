fdd5321811bf06b40d1ceeddee3cab5367113778
John Joly <john_joly@icloud.com>
2024-09-26 22:39:09 +0400

    add telegram scirpt and bootstrap css

---
+++ js/telegram.js
@@ -0,0 +1,4 @@
+window.replainSettings = { id: 'f0a20e49-8b59-4592-bc9c-83b08d4baed3' };
+(function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;
+var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+})('https://widget.replain.cc/dist/client.js');
