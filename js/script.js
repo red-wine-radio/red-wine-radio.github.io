7dca10de72004f05fa7b0931c8633802c7b89b07
John Joly <john_joly@icloud.com>
2025-04-29 21:01:48 +0400

    block scroll

--- js/script.js
+++ js/script.js
@@ -175,3 +175,16 @@ function changeLogo(newSrc) {
 eventSource.addEventListener('ping', function() {
     console.log('Ping received');
 });
+
+// Блокировка жестов масштабирования на мобильных устройствах
+document.addEventListener('gesturestart', function (e) {
+    e.preventDefault();
+});
+
+document.addEventListener('gesturechange', function (e) {
+    e.preventDefault();
+});
+
+document.addEventListener('gestureend', function (e) {
+    e.preventDefault();
+});
